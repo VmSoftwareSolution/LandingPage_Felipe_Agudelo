79fc0194ff64b492c5e1f80356ba96a76e0163e5
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-06-14 10:50:16 -0500

    CONFIGURATION(commits): Configurated to husky and commitline

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,29 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [2, 'always', [
-        'FEATURE',
-        'FIX',
-        'DOCS',
-        'REFACTOR',
-        'TEST',
-        'CI',
-        'HOTFIX',
-        'CHORE',
-        'RELEASE',
-        'SP',
-        'BUGREPORT',
-        'MERGE',
-        'STYLES',
-        'CONFIGURATION',
-      ]],
-      'subject-case': [2, 'always', 'sentence-case'],
-    },
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'FEATURE',
+      'FIX',
+      'DOCS',
+      'REFACTOR',
+      'TEST',
+      'CI',
+      'HOTFIX',
+      'CHORE',
+      'RELEASE',
+      'SP',
+      'BUGREPORT',
+      'MERGE',
+      'STYLES',
+      'CONFIGURATION',
+    ]],
+    'type-case': [2, 'always', 'upper-case'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100]
+  }
+};
