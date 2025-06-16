2e6387ef7f5cd32ae524d52253514adfd1247536
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-06-16 11:08:45 -0500

    FEATURE(navbar): Creacion del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './core/layouts/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Navbar,
+  }
+];
