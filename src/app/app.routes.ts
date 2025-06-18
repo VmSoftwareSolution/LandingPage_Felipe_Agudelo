a3371c321263a26fe2b6b3ea3124b9e7380182a8
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-06-17 19:13:35 -0500

    FIX(navbar): Solucion bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,5 @@ import { Routes } from '@angular/router';
 import { Navbar } from './core/layouts/navbar/navbar';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: Navbar,
-  }
+
 ];
