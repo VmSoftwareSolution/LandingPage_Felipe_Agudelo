785eedbed729392ea7850389b90714362117cf3d
JuanEOL <juaneteo25@gmail.com>
2025-07-01 01:56:15 -0500

    FEATURE(landi-13): Se acabo el modulo de about me completo

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+  PLATFORM_ID,
+  inject 
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-about',
@@ -6,6 +14,13 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class About {
 
+export class About implements OnInit{
+   private platformId = inject(PLATFORM_ID);
+   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 }
+
