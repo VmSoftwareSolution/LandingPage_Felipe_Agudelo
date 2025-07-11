4fcd107301be6d42c446b508ca8db8ad73a8d96a
JuanEOL <juaneteo25@gmail.com>
2025-07-11 02:18:37 -0500

    FEATURE(landi-25): Se termino el footer

--- src/app/pages/landinpage/landinpage.ts
+++ src/app/pages/landinpage/landinpage.ts
@@ -4,10 +4,11 @@ import { Portfolio } from "../portfolio/portfolio";
 import { Scroll } from "../../shared/scroll/scroll";
 import { About } from '../about/about';
 import { Contact } from '../contact/contact';
+import { Footer } from '../../core/layouts/footer/footer';
 
 @Component({
   selector: 'app-landinpage',
-  imports: [Home, Portfolio, Scroll, About , Contact],
+  imports: [Home, Portfolio, Scroll, About , Contact, Footer],
   templateUrl: './landinpage.html',
   styleUrl: './landinpage.css'
 })
