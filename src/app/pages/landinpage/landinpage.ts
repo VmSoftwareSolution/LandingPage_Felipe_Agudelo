9f58981cf0f894a8d32036517426312dc0c8bfaf
JuanEOL <juaneteo25@gmail.com>
2025-07-26 00:46:21 -0500

    FEATURE(landi-22): Se completo testimonios

--- src/app/pages/landinpage/landinpage.ts
+++ src/app/pages/landinpage/landinpage.ts
@@ -5,10 +5,11 @@ import { Scroll } from "../../shared/scroll/scroll";
 import { About } from '../about/about';
 import { Contact } from '../contact/contact';
 import { Footer } from '../../core/layouts/footer/footer';
+import { Testimonial } from '../testimonial/testimonial';
 
 @Component({
   selector: 'app-landinpage',
-  imports: [Home, Portfolio, Scroll, About , Contact, Footer],
+  imports: [Home, Portfolio, Scroll, About , Contact, Footer, Testimonial],
   templateUrl: './landinpage.html',
   styleUrl: './landinpage.css'
 })
